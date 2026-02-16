7c44bd8906e24cc2674f6c69b702150ec2fb91c1
Amirreza <amirrezakryml@gmail.com>
2026-02-16 14:02:15 +0330

    fix: google font issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { cookies } from "next/headers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "nextjs dashboard",
@@ -32,10 +22,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
-        cz-shortcut-listen="true"
-      >
+      <body className={` antialiased flex`} cz-shortcut-listen="true">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
